0040fb9c5031cb79b8a849c9dbd966eb27f69cea
igorgromykojs <125064212+igorgromykojs@users.noreply.github.com>
2023-04-14 14:59:04 +0300

    feat: make queue

--- src/queue.js
+++ src/queue.js
@@ -13,24 +13,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
 class Queue {
-
+  constructor (){
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.first;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    let newNode = new Node(value);
+    if(!this.first){
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next=newNode;
+      this.last=newNode;
+    }
+    return ++this.size;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.first) return null
+    const temp = this.first;
+    if(this.first===this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
   }
 }
-
 module.exports = {
   Queue
 };
