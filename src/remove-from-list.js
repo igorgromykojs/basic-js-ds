974ad50398b77ea3e9f3259c6e8c76c719aebda4
igorgromykojs <125064212+igorgromykojs@users.noreply.github.com>
2023-04-17 21:19:45 +0300

    fear: change folder

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,10 +22,91 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+  addToTheEnd(value) {
+    let node = new Node(value); //creating the node using class Node
+
+    if (this.length === 0) {
+        this.head = node; // If there are no nodes 
+        // node variable will be the first and head node in the list
+    } else {
+        let current = this.head;
+
+        while(current.next) {
+            current = current.next;
+        }
+
+        current.next = new Node(value);
+    }
+
+    this.length++;
+  }
+  getIndexOf(value) {
+    let current = this.head; // current is a head of our list
+    let index = 0; // index which will be returned
+
+    while(current) {
+        if (current.value === value) {
+            return index;
+        }
+        
+        current = current.next;
+        index++;
+    }
+
+    return -1;
+  }
+  removeElementByValue(value) {
+    return this.removeFromPosition(this.getIndexOf(value));
+  }
+}
+const q = new LinkedList();
+q.addToTheEnd(5);
+q.addToTheEnd(7);
+q.removeElementByValue(5);
+console.log(q);
+
+// function removeKFromList(l, k) {
+//   //figure out the head of the new list so we can reference it later
+//   var head = l;
+
+//   while (head.value === k){
+//       head = head.next;
+//   }
+
+//   var node = head;
+//   var temp = null;
+
+//   while (node && node !== null) {
+//       if (node.next.value === k){
+//           temp = node.next.next;
+//           node.next.next = null;
+//           node.next = temp;
+//       }
+//       node = node.next; 
+//       console.log("+++", head);
+//   }
+
+//   console.log("---", head);  
+// }
+
+
+
+// console.log(removeKFromList([3, 1, 2, 3, 4, 5], 3));
+// function removeKFromList(/* l, k */) {
+//   throw new NotImplementedError('Not implemented');
+//   // remove line with error and write your code here
+// }
 // function removeKFromList(l, k) {
 //   let res = l.filter(item => item != k);
 //   return res;
